27a8bd91943e41658903a5c02a56145269df6351
caglar.yalcin <caglar.yalcin41@gmail.com>
2019-11-26 20:15:38 +0300

    devam

---
+++ INote.API/Dtos/PostNoteDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace INote.API.Dtos
+{
+    public class PostNoteDto
+    {
+        [Required(ErrorMessage ="Başlık alanı zorunludur")]
+        [StringLength(50, ErrorMessage ="Başlık çok uzun olmuş")]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+    }
+}
--- INote.API/Migrations/Configuration.cs
+++ INote.API/Migrations/Configuration.cs
@@ -1,6 +1,10 @@
 namespace INote.API.Migrations
 {
+    using INote.API.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -18,6 +22,38 @@ namespace INote.API.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+            if (!context.Users.Any())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                var user = new ApplicationUser
+                {
+                    UserName = "caglaryalcin41@gmail.com",
+                    Email = "caglaryalcin41@gmail.com",
+                    EmailConfirmed = true
+                };
+
+                userManager.Create(user, "Ankara1.");
+                user.Notes = new HashSet<Note>();
+
+                user.Notes.Add(new Note
+                {
+                    Title = "Sample Note 1",
+                    Content = "Kocaelim benim biricik sevgilim söyle senden baþka kimim var benim seninle aðlarým seninle gülerim söyle senden baþka kimim var benim",
+                    CreatedTime = DateTime.Now
+                });
+
+                user.Notes.Add(new Note
+                {
+                    AuthorId = user.Id,
+                    Title = "Sample Note 2",
+                    Content = "Gönlümde sadece körfezim yatar, bütün gemiler ise körfezde batar, bu alemde kimse baðlamaz bizi burasý basra deðil izmit körfezi. ",
+                    CreatedTime = DateTime.Now
+                });
+
+                context.Entry(user).State = EntityState.Modified;
+            }
         }
     }
 }
